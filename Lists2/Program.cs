49307ce7e93cec448090322e60163709cbcf3bb5
AbdullahVC <69648705+AbdullahVC@users.noreply.github.com>
2024-09-09 02:06:06 +0300

    Add project files.

---
+++ Lists2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> kahveler = new List<string>();
+
+        Console.WriteLine("Lütfen 5 kahve ismi girin:");
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write("Kahve " + (i + 1) + ": ");
+            string kahveIsmi = Console.ReadLine();
+            kahveler.Add(kahveIsmi);
+        }
+
+        Console.WriteLine("\nGirdiğiniz kahveler:");
+        foreach (string kahve in kahveler)
+        {
+            Console.WriteLine("- " + kahve);
+        }
+    }
+}
